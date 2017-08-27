46de89ccacbe34ea2c351cd0e87456135561d696
Korobov <korobov.nnov@gmail.com>
2017-08-27 03:26:24 -0400

    Initial Commit

---
+++ TipAssistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipAssistant
+//
+//  Created by Дмитрий Коробов on 27.08.17.
+//  Copyright © 2017 Дмитрий Коробов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
