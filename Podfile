3c1036723395e1c4b2aa5c7381a76e279856215f
Korobov <korobov.nnov@gmail.com>
2017-09-19 18:17:42 -0400

    Set crashlytics 2

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '9.0'
+
+target 'TipAssistant' do
+  use_frameworks!
+
+  pod 'Fabric'
+  pod 'Crashlytics'
+end
---
+++ Pods/Target Support Files/Pods-TipAssistant/Pods-TipAssistant-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TipAssistant : NSObject
+@end
+@implementation PodsDummy_Pods_TipAssistant
+@end
